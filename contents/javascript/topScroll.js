aea8315b4234fb5d2ec4988326a2f71583e4ba97
TobiaApolloni <tobia.apolloni@gmail.com>
2018-02-09 21:05:05 +0100

    cose

--- contents/javascript/topScroll.js
+++ contents/javascript/topScroll.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
         document.getElementById("topScrollButton").style.display = "block";
     } else {
         document.getElementById("topScrollButton").style.display = "none";
